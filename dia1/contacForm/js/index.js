2ce501bddf7d378d1ebc429c96f8238025dc8451
Marianella08 <marytore08@gmail.com>
2023-06-23 20:42:59 -0500

    arreglos de sintaxis

--- dia1/contacForm/js/index.js
+++ dia1/contacForm/js/index.js
@@ -2,12 +2,12 @@ const openModal = document.querySelector('.modal_open')
 const modal = document.querySelector('.modal')
 const closeModal = document.querySelector('.modal_close')
 
-openModal.addEventListener('click', (e)=>{
+openModal.addEventListener('click', (e) => {
     e.preventDefault()
     modal.classList.add('modal--show')
 })
 
-closeModal.addEventListener('click', (e)=>{
+closeModal.addEventListener('click', (e) => {
     e.preventDefault()
     modal.classList.remove('modal--show')
 })
