df5337bbcd7b07fbf3a19573946e9ff39175615a
Marianella08 <marytore08@gmail.com>
2023-07-07 12:31:58 -0500

    Listo

--- dia3/PalletGenerator/js/index.js
+++ dia3/PalletGenerator/js/index.js
@@ -1,6 +1,6 @@
-const form = document.querySelector("form");
-const input = document.querySelector("#hex");
-const colorDiv = document.querySelector("#colors");
+const form = document.querySelector("form")
+const input = document.querySelector("#hex")
+const colorsDiv = document.querySelector("#colors")
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -8,12 +8,11 @@ form.addEventListener("submit", (event) => {
     const hex = input.value;
 
     if (!isValidHex(hex)) {
-        alert("ingrese un código hex correcto.")
+        alert("Ingrese un código hexadecimal válido.");
         return;
     }
 
     const colors = generateColors(hex);
-
     renderColors(colors);
 });
 
@@ -21,56 +20,56 @@ function isValidHex(hex) {
     return /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(hex);
 }
 
-function generateColors(hex) {
+function generateColors (hex){
     const baseRgb = hexToRgb(hex);
     const colorVariations = [
-        { r: -30, g: 0, b: 0 },
-        { r: 0, g: -30, b: 0 },
-        { r: 0, g: 0, b: -30 },
-        { r: 30, g: 0, b: 0 },
-        { r: 0, g: 30, b: 0 },
-        { r: 0, g: 0, b: 30 },
+        {r: -30, g: 0, b: 0},
+        {r: 0, g: -30, b: 0},
+        {r: 0, g: 0, b: -30},
+        {r: 30, g: 0, b: 0},
+        {r: 0, g: 30, b: 0},
+        {r: 0, g: 0, b: 30},
     ];
 
-    const colors = colorVariations.map((variation) => {
+    const colors = colorVariations.map((variation) =>{
         return {
-            r: Math.min(225, Math.max(0, baseRgb.r + variation.r)),
-            g: Math.min(225, Math.max(0, baseRgb.g + variation.g)),
-            b: Math.min(225, Math.max(0, baseRgb.b + variation.b)),
-        }
+            r: Math.min(255, Math.max(0, baseRgb.r + variation.r)),
+            g: Math.min(255, Math.max(0, baseRgb.g + variation.g)),
+            b: Math.min(255, Math.max(0, baseRgb.b + variation.b)),
+        };
     });
 
     return colors;
 }
-
 
 function hexToRgb(hex) {
     const r = parseInt(hex.substring(1, 3), 16);
     const g = parseInt(hex.substring(3, 5), 16);
     const b = parseInt(hex.substring(5, 7), 16);
 
-    return (r, g, b);
+    return { r, g, b };
 }
 
-function renderColors(colors) {
-    colorDiv.innerHTML = "";
+function renderColors(colors){
+    colorsDiv.innerHTML = "";
 
-    colors.forEach(color => {
-        const colorBox = document.createElement("div");
-        colorBox.className = "color-box";
-        colorBox.style.backgroundColor = `rgb(${color.r}, ${color.g}, ${color.b})`;
+    colors.forEach((color) => {
+        const colorbox = document.createElement("div");
+        colorbox.className = "color-code";
+        colorbox.style.backgroundColor = `rgb(${color.r}, ${color.g}, ${color.b})`;
 
         const colorCode = document.createElement("div");
         colorCode.className = "color-code";
-        colorCode.innerText = `RGB: ${color.r}, ${color.g}, ${color.b
-            }\nHEX: ${rgbToHex(color.r, color.g, color.b)}`;
+        colorCode.innerText = `RGB: ${color.r}, ${color.g}, ${
+            color.b
+        }\nHEX: ${rgbToHex(color.r, color.g, color.b)}`;
 
-        colorBox.appendChild(colorCode);
-        colorDiv.appendChild(colorBox);
+        colorbox.appendChild(colorCode);
+        colorsDiv.appendChild(colorbox);
     });
 }
 
-function rgbToHex(r, g, b) {
+function rgbToHex(r, g, b){
     const hexR = r.toString(16).padStart(2, "0");
     const hexG = g.toString(16).padStart(2, "0");
     const hexB = b.toString(16).padStart(2, "0");
